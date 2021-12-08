0cbfa5c6e1810b7a779b0c54a0cc90acea548961
Simon Gravelle <simon.gravelle@live.fr>
2021-12-08 20:22:22 +0100

    update nearest neighbor search

--- nearest_neighbor_search.py
+++ nearest_neighbor_search.py
@@ -1,15 +1,14 @@
 import copy
 import numpy as np
+from numpy.linalg import norm
 def neighborsearch(neighbor,molecule,cptatm, x, y, z, Lx, Ly, Lz):
-    '''Search neighbor in a box and return the closest distance found'''
-    mind = 10
-    XYZ = copy.deepcopy(neighbor.T[0:cptatm].T)
-    for xx in [-Lx,0,Lx]:
-        for yy in [-Ly,0,Ly]:
-            for zz in [-Lz,0,Lz]:
-                XYZ = np.append(XYZ.T,neighbor.T[0:cptatm]+np.array([xx,yy,zz]), axis=0).T
-    XYZ = XYZ.T[cptatm:].T
+    '''Search all neighbor to a molecule in a box and return the closest distance'''
+    box = np.array([Lx, Ly, Lz])
+    minr = 10
     for m in molecule.T:
-        d = np.sqrt((m[0]+x-XYZ[0])**2+(m[1]+y-XYZ[1])**2+(m[2]+z-XYZ[2])**2)
-        mind = np.min([np.min(d),mind])
-    return mind
+        x0 = m[0] + x
+        y0 = m[1] + y
+        z0 = m[2] + z
+        dxdydz = np.remainder(neighbor[:cptatm].T - np.array([x0,y0,z0]) + box/2., box) - box/2.
+        minr = np.min([minr,np.min(norm(dxdydz,axis=1))])
+    return minr
